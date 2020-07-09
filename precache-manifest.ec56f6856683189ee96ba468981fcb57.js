666c3addc1e9ffa214ca8b55ef916ddd09e205d3
Nandini226 <60805353+Nandini226@users.noreply.github.com>
2020-07-09 20:46:03 +0530

    Updates

---
+++ precache-manifest.ec56f6856683189ee96ba468981fcb57.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b82e7a5b6b725488b80069b6415b48",
+    "url": "/inscription/index.html"
+  },
+  {
+    "revision": "27807c8dbc3737126622",
+    "url": "/inscription/static/css/main.31334d18.chunk.css"
+  },
+  {
+    "revision": "35c0bdfa98b524b8378f",
+    "url": "/inscription/static/js/2.8c4af460.chunk.js"
+  },
+  {
+    "revision": "be14b386ccc34a49dee76740150eacbc",
+    "url": "/inscription/static/js/2.8c4af460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27807c8dbc3737126622",
+    "url": "/inscription/static/js/main.ec0a7d52.chunk.js"
+  },
+  {
+    "revision": "db19d6963d50c4514c9b",
+    "url": "/inscription/static/js/runtime-main.3dbcd77b.js"
+  },
+  {
+    "revision": "d3c1bf32b62f4912728968b1a6990d32",
+    "url": "/inscription/static/media/KR Declaration.d3c1bf32.ttf"
+  },
+  {
+    "revision": "c65e44c357863b2050379d0db0ab0d63",
+    "url": "/inscription/static/media/KR ReadMe.c65e44c3.txt"
+  },
+  {
+    "revision": "91dfc7ce94bb62fc98820a136b0072be",
+    "url": "/inscription/static/media/img.91dfc7ce.jpg"
+  },
+  {
+    "revision": "f7d97046d1a47dc84869fa316f86542d",
+    "url": "/inscription/static/media/img1.f7d97046.jpg"
+  },
+  {
+    "revision": "8140331c63238635466f022498347773",
+    "url": "/inscription/static/media/img2.8140331c.jpg"
+  },
+  {
+    "revision": "028f104fe4e6cee852b8253e22d2efe8",
+    "url": "/inscription/static/media/img3.028f104f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inscription/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e17f8f77b49fc3242287a398de681c2c",
+    "url": "/inscription/static/media/template10.e17f8f77.jpg"
+  },
+  {
+    "revision": "304fc22021e3bc6d8e22916fde63f2fd",
+    "url": "/inscription/static/media/template12.304fc220.jpg"
+  },
+  {
+    "revision": "99362e75596a6d4837f3735feb7f9d08",
+    "url": "/inscription/static/media/template13.99362e75.jpg"
+  },
+  {
+    "revision": "4d7c4a5112c836505ebd7bb17f045b61",
+    "url": "/inscription/static/media/template14.4d7c4a51.jpg"
+  },
+  {
+    "revision": "1ae2855fd730864efeb63e8c64267907",
+    "url": "/inscription/static/media/template2.1ae2855f.jpg"
+  },
+  {
+    "revision": "1f5a62ae26a61f0cf3c410b20098e9e1",
+    "url": "/inscription/static/media/template3.1f5a62ae.jpg"
+  },
+  {
+    "revision": "4c272d06ebc4418acd86ad9d0909c6c1",
+    "url": "/inscription/static/media/template6.4c272d06.jpg"
+  },
+  {
+    "revision": "028f104fe4e6cee852b8253e22d2efe8",
+    "url": "/inscription/static/media/template7.028f104f.jpg"
+  },
+  {
+    "revision": "bee348498248644a977069bc6d86df18",
+    "url": "/inscription/static/media/template8.bee34849.jpg"
+  }
+]);
